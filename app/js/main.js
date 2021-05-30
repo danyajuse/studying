e7e1a8de6a298d75b7f6c252714ea21f9e4d6a02
danya-juse <danialisterman@gmail.com>
2021-05-30 14:43:18 +0300

    hw13

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,32 @@
 $(function() {
+
+  $(window).on("scroll", function () {
+    $('header').toggleClass("header--active", $(this).scrollTop() > $(window).height());
+  });
   
-  $('.blog__slider').slick({
+  $('.trending__slider').slick({
     dots: true,
     arrows: false,
   });
 
-  var mixer = mixitup('.product__items');
 
+  const mixin1 = document.querySelector('.weak-products__items');
+  const mixin2 = document.querySelector('.design-products__items');
+
+  if (mixin1) {
+    mixitup('.weak-products__items', {
+      selectors: {
+        control: '.filter'
+      }
+      
+    })
+  }
+
+  if (mixin2) {
+    mixitup('.design-products__items', {
+      selectors: {
+        control: '.filter2'
+      }
+    })
+  }
 })
